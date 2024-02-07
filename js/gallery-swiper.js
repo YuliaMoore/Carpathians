95d9864bc5104068ac4f1e832063b74860d6e649
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2024-02-07 22:26:12 +0200

    1

---
+++ js/gallery-swiper.js
@@ -0,0 +1,18 @@
+import Swiper from "swiper";
+// // import { Navigation, Pagination } from "swiper/modules";
+// // import Swiper and modules styles
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+new Swiper(".swiperGallery ");
+// , {
+//   direction: "horizontal",
+// loop:false,
+//   allowTouchMove: true,
+//   slidesPerView: 2,
+//   spaceBetween: 30,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// }
